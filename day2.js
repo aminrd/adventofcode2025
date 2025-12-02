6c73a308ac62e227348909266e7b14dee4318e36
Amin <amin@Amins-MacBook-Pro.local>
2025-12-01 20:59:21 -0800

    preparing day 2

---
+++ day2.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const args = process.argv.slice(2); // remove node and script path
+const debug = args.includes("-debug"); // true if -debug is passed
+let filename = debug ? "test.txt" : "day1.txt";
+
